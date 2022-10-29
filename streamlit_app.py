25cf431b5d04159a04cff50cb33ec29afee214c4
Nicole <33319199+nhamler@users.noreply.github.com>
2022-10-29 13:58:53 -0500

    Update streamlit_app.py
    Add subtitle and body

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.subtitle('Breakfast Menu')
+
+streamlit.body('Omega 3 & Blueberry Oatmeal'
+                'Kale, Spinach & Rocket Smoothie'
+               'Hard-Boiled Free-Range Egg')
