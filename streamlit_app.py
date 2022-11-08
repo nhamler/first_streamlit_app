5d8bac66952026b201dcd71b5ccdfb54e838a432
Nicole <33319199+nhamler@users.noreply.github.com>
2022-11-07 18:26:15 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,21 +7,20 @@ from urllib.error import URLError
 
 streamlit.title('View Our Fruit List - Add Your Favorites!')
 
-streamlit.header('Breakfast Menu')
+#streamlit.header('Breakfast Menu')
 
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+#streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+#streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+#streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+#streamlit.text('🥑🍞 Avocado Toast')
 
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 #import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macfros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
